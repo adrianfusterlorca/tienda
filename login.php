c217231fc4f7a8ba9ea04f31217c526c10191c36
Adrian Fuster <adrianfuster@elcampico.org>
2024-06-11 15:30:37 +0200

    Created basic login and signup forms

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="styles/signUp.css">
+</head>
+<body>
+    <div class="form">  
+        <form action="" method="post">
+            <input type="text" placeholder="Email" name="email" id="">
+            <input type="text" placeholder="Contraseña" name="password" id="">
+            <span>¿Recordar contraseña?</span>
+            <input type="checkbox" name="rememberMe" id="">
+            <button type="submit" name="login">Entrar</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="styles/signUp.css">
+</head>
+<body>
+    <div class="form">
+        <form action="" method="post">
+            <input type="text" placeholder="Nombre" name="nombre" id="">
+            <input type="text" placeholder="Apellidos" name="apellidos" id="">
+            <input type="text" placeholder="Email" name="email" id="">
+            <input type="text" placeholder="Contraseña" name="password" id="">
+            <input type="text" placeholder="Confirmar Contraseña" name="confirmPass" id="">
+            <button type="submit" name="signUp">Registrar</button>
+        </form>
+    </div>
+</body>
+</html>
